36b68c289c9ce140626c74cef4a5201edda9051e
Nick Robson <robson.nicholas1@gmail.com>
2016-02-04 13:18:57 +0800

    debugging

--- src/main/java/xyz/nickr/jitter/JitterBayeux.java
+++ src/main/java/xyz/nickr/jitter/JitterBayeux.java
@@ -189,6 +189,7 @@ public class JitterBayeux {
         subscribe(resolve(ROOM_MESSAGES, mapOf(new String[][]{ {"roomId", room.getID()} })), (ch, msg) -> {
             JSONObject model = new JSONObject(msg).getJSONObject("data").getJSONObject("model");
             System.out.println(model);
+            System.out.println(model.optJSONObject("fromUser"));
             jitter.onMessage(new MessageImpl(jitter, room, model));
         });
     }
